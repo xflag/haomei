c676408b06bd43f9ed5331d48761b87887b13537
xuyq <xuyq_man@163.com>
2015-01-29 00:03:07 +0800

    修复未连网时，直接退出的bug

--- src/com/haomei/app/adapter/ForecastGridAdapter.java
+++ src/com/haomei/app/adapter/ForecastGridAdapter.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
